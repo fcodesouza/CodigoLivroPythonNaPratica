b261dda83544161b316ee39acd40c5366f3051e6
De Souza <51327629+fcodesouza@users.noreply.github.com>
2022-04-20 22:08:02 -0300

    Add files via upload

---
+++ 024.py
@@ -0,0 +1,7 @@
+num1 = input('Digite um número: ')
+num2 = input('Digite outro número: ')
+
+if num1 > num2:
+    print('N1 é maior que N2.')
+else:
+    print('N2 é maior que N1.')
---
+++ 025.py
@@ -0,0 +1,6 @@
+num1 = int(input('Digite um número: '))
+
+if num1 % 2 == 0:
+    print('N1 é par.')
+else:
+    print('N1 é impar.')
